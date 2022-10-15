42fa06fbdd77bd25b56cecce38535b32e53689d1
nokko-nokko <tatsu24hs@gmail.com>
2022-10-15 23:51:02 +0900

    コンフリクト解消

--- app/app.py
+++ app/app.py
@@ -1,18 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template("home.html")
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-@app.route("/howto")
+@app.route('/howto')
 def howto():
-    return render_template("howto.html")
-
-@app.route("/result")
+    return render_template('howto.html')
+    
+@app.route('/result')
 def result():
-    return render_template("result.html")
+    return render_template('result.html')
 
+
+    #おまじない
 if __name__ == "__main__":
     app.run(debug=True)
