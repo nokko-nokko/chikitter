9cbe8e6e133f5592bdcfc4d68b279f5bcc6bc77a
nokko-nokko <tatsu24hs@gmail.com>
2022-10-19 00:37:27 +0900

    ちきーとで定型文をランダムに表示するようにした

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import random
+from flask import Flask, render_template, request
+from models.models import PositiveContent
 
 app = Flask(__name__)
 
@@ -10,10 +12,23 @@ def home():
 def howto():
     return render_template('howto.html')
     
-@app.route('/result')
+@app.route('/result', methods=["GET","POST"])
 def result():
-    return render_template('result.html')
-
+    if request.method == "POST":
+        chickeet = request.form["chickeet_content"]
+        positivelist = []
+        all_positivelist = []
+        all_positive_queries = PositiveContent.query.all()
+        for positive_query in all_positive_queries:
+            all_positivelist.append(positive_query.body)
+        all_positivelist_random = random.sample(all_positivelist, k=6)
+        for i in range(6):
+            positivelist.append(all_positivelist_random[i])
+        return render_template('result.html',chickeet=chickeet,positivelist=positivelist)
+    else:
+        chickeet = "おにぎりは鮭しか勝たん"
+        positivelist = ["やるやん","僕もそう思います","おにぎり最高！","時代は鮭","まじで同感","おいしいよね"]
+        return render_template('result.html',chickeet=chickeet,positivelist=positivelist)
 
     #おまじない
 if __name__ == "__main__":
