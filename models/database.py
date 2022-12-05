a2df01c23b3d6cc95cfa927cea57eebc828a31aa
nokko-nokko <tatsu24hs@gmail.com>
2022-12-05 20:24:59 +0900

    PostgreSQLに戻した

--- models/database.py
+++ models/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 import os
 
 database_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'positive.db')
-engine = create_engine('sqlite:///' + database_file, convert_unicode=True)
+engine = create_engine('postgresql://localhost/' + database_file, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
